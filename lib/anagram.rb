595f57e6b0296ed2e1446cfa3eac682a67551be1
nicholas shawn freeman <nick_freeman65@yahoo.com>
2022-02-11 16:09:54 -0600

    commit tree 2218bcda5e602ecc2bdc33c0531c15f4d58f8646

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+    
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(phrase)
+        word_chars = word.chars
+        find = []
+        phrase.map do |b|
+            if word_chars.sort == b.chars.sort
+                find.push(b)
+            else puts "no"
+            end
+        end
+        find
+    end
+end
+
